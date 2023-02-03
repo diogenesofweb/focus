1929d53fa569ebb7d746fa80ff56b857d0bd47d5
Diogenes <diogenesofweb@gmail.com>
2023-02-03 09:57:47 +0200

    up

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+// import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({ postcss: true }),
+	// preprocess: preprocess({ postcss: true }),
 	kit: {
 		adapter: adapter({
 			pages: 'build',
