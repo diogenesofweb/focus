f2c93cc324288cac78b4e2e71311e8697d55b6c6
Diogenes <diogenesofweb@gmail.com>
2023-06-23 09:54:57 +0300

    svelte v4

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-nesting')
+  ]
+}
+
+module.exports = config
+
