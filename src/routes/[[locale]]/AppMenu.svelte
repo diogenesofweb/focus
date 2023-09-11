4ba3fe0607ed3b1696924f9cf3e35ed2a30a3d57
Diogenes <diogenesofweb@gmail.com>
2023-09-11 09:22:19 +0300

    Up: 2023-09-11 09:22:19

--- src/routes/[[locale]]/AppMenu.svelte
+++ src/routes/[[locale]]/AppMenu.svelte
@@ -11,21 +11,22 @@
 		[
 			l.t.menu.timers,
 			[
-				{ href: '/pomodoro', name: l.t.timers.pomodoro.h },
-				{ href: '/alarm-clock', name: l.t.timers.alarm_clock.h },
-				{ href: '/countdown', name: l.t.timers.countdown.h },
-				{ href: '/countdown/to', name: l.t.timers.countdown_date.h },
-				{ href: '/stopwatch', name: l.t.timers.stopwatch.h }
+				{ href: pre + '/pomodoro', name: l.t.timers.pomodoro.h },
+				{ href: pre + '/alarm-clock', name: l.t.timers.alarm_clock.h },
+				{ href: pre + '/countdown', name: l.t.timers.countdown.h },
+				{ href: pre + '/countdown/to', name: l.t.timers.countdown_date.h },
+				{ href: pre + '/stopwatch', name: l.t.timers.stopwatch.h }
 			]
 		],
+
 		[
 			l.t.menu.pomodoro,
 			[
-				{ href: '/pomodoro/sequences', name: l.t.r.sequences.body.h },
-				{ href: '/pomodoro/break-activities', name: l.t.r.breaks.body.h },
-				{ href: '/pomodoro/radio-stations', name: l.t.r.radios.body.h },
-				{ href: '/pomodoro/stats', name: l.t.r.stats.body.h },
-				{ href: '/pomodoro/snaps', name: l.t.r.snaps.body.h }
+				{ href: pre + '/pomodoro/sequences', name: l.t.r.sequences.body.h },
+				{ href: pre + '/pomodoro/break-activities', name: l.t.r.breaks.body.h },
+				{ href: pre + '/pomodoro/radio-stations', name: l.t.r.radios.body.h },
+				{ href: pre + '/pomodoro/stats', name: l.t.r.stats.body.h },
+				{ href: pre + '/pomodoro/snaps', name: l.t.r.snaps.body.h }
 			]
 		],
 
@@ -33,16 +34,24 @@
 			l.t.menu.tools,
 			[
 				{
-					href: '/tools/date-to-date-calculator',
+					href: pre + '/tools/date-to-date-calculator',
 					name: l.t.r.date2date.head.title
 				},
 				{
-					href: '/tools/add-or-subtract-date',
+					href: pre + '/tools/add-or-subtract-date',
 					name: l.t.r.add_sub_date.head.title
 				}
 			]
 		]
 	];
+
+	if (l.r.locale === 'ua') {
+		// console.log('ua');
+		links[2][1].push({
+			href: 'https://www.kursvalyut.top/tools/currency-converter',
+			name: 'Конвертер Валют'
+		});
+	}
 
 	/** @type {HTMLDialogElement} */
 	let dialog;
@@ -71,11 +80,7 @@
 				<ul>
 					{#each el[1] as { href, name }}
 						<li>
-							<a
-								href={pre + href}
-								class="btn text round"
-								on:click={() => dialog.close()}
-							>
+							<a {href} class="btn text round" on:click={() => dialog.close()}>
 								{name}
 							</a>
 						</li>
